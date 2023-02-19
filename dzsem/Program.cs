30bc0a9a4bbb0c006c83786c647af9130cc2ef56
Aleksandr <Aleksandr_Stepanov_1995@mail.ru>
2023-02-19 21:26:30 +0300

    Начало дз

---
+++ dzsem/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+int resaul = num1;
+
+if (num1 > 10000 && num1 < 99999)
+{
+    while (num1 > 0)
+    {
+        num2 *= 10;
+        num2 += num1 % 10;
+        num1 /= 10;
+
+    }
+    if (resaul == num2)
+
+
+        Console.Write("является палиндромом " + num2);
+    else
+    {
+        Console.Write("не является палиндромом" + num1);
+    }
+
+}
+else
+{
+    System.Console.WriteLine("некорректное число");
+}
